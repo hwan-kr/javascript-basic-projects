d5f9eb9333f6fdc21c9e0218e38af8b2bb1c62c9
hwan-kr <fabronjeon@naver.com>
2024-02-12 05:51:49 +0900

    24-02-12 05:51 color-flipper complete

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,18 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+
+const clickBtn = document.getElementById('btn');
+const backgroundColor = document.querySelector('main');
+const colorTag = document.querySelector('span');
+
+// num1 ~ num2-1 사이 숫자 생성.
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function changeColor() {
+    let colorIndex = colors[randomNumber(0, 4)];
+    backgroundColor.style.backgroundColor = colorIndex;
+    colorTag.textContent = colorIndex;
+}
+
+clickBtn.addEventListener('click', changeColor);
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,27 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']; // 16개
+
+const clickBtn = document.getElementById('btn');
+const backgroundColor = document.querySelector('main');
+const colorTag = document.querySelector('span');
+
+let newColorCode = [];
+let colorCode;
+
+// num1 ~ num2-1 사이 숫자 생성.
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function makeColor() {
+    newColorCode = [];
+    let element;
+    for (i = 0; i < 6; i++) {
+        element = hex[randomNumber(0, 15)];
+        newColorCode.push(element);
+    }
+    colorCode = newColorCode.join('');
+    backgroundColor.style.backgroundColor = `#${colorCode}`;
+    colorTag.textContent = `#${colorCode}`;
+}
+
+clickBtn.addEventListener('click', makeColor);
