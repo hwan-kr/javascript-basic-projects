8d26689df04dba1965ec5b68ba472b9112cf981e
hwan-kr <fabronjeon@naver.com>
2024-02-12 07:10:55 +0900

    02-counter 각 버튼 요소 만듦

---
+++ 02-counter/setup/app.js
@@ -0,0 +1,7 @@
+const decreaseBtn = document.getElementById('decrease');
+const resetBtn = document.getElementById('reset');
+const increaseBtn = document.getElementById('increase');
+
+decreaseBtn.addEventListener('click');
+resetBtn.addEventListener('click');
+increaseBtn.addEventListener('click');
